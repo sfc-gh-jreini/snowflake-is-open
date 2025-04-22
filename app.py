70a2cbf62fa6f9d4ed039f6826d360ed599e4338
Josh Reini <joshua.reini@snowflake.com>
2025-04-22 13:49:24 -0400

    write to snow

--- app.py
+++ app.py
@@ -90,9 +90,9 @@ if st.button("✅ Check My Matches"):
         user=os.getenv("SNOWFLAKE_USER"), 
         password=os.getenv("SNOWFLAKE_PASSWORD"), 
         account=os.getenv("SNOWFLAKE_ACCOUNT"), 
-        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"), 
-        database=os.getenv("SNOWFLAKE_DATABASE"), 
-        schema=os.getenv("SNOWFLAKE_SCHEMA")
+        warehouse="COMPUTE_WH", 
+        database="OPEN_SNOW", 
+        schema="PUBLIC"
     )
     cursor = conn.cursor()
 
